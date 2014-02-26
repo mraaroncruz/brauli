346a70b0fa071e129e5ca59a7b4c60eb227219f0
Aaron Cruz <aaron@aaroncruz.com>
2014-02-26 13:38:11 +0100

    Added map with markers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,27 @@ angular.module('starter.controllers', [])
 .controller('BreweryIndexCtrl', function($scope, Brewery) {
   // "Brewerys" is a service returning mock data (services.js)
   $scope.breweries = Brewery.all_with_geo();
+
+  $scope.currentLocation = {
+    latitude: 48.288158,
+    longitude: 16.308020
+  }
+
+  getCenter = function () {
+    return $scope.currentLocation
+  }
+
+  $scope.map = {
+    center: getCenter(),
+    zoom: 10,
+    options: {
+      scaleControl: false,
+      streetViewControl: false,
+      zoomControl: false,
+      mapTypeControl: false
+    },
+    markers: $scope.breweries
+  };
 })
 
 
